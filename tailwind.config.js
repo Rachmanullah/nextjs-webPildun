412e1294a643c6072eef614fdd7317729dd2ccb2
Rachmanullah <rachmanullah1@gmail.com>
2023-07-31 00:43:25 +0700

    Section Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,13 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1",
+        lg: "5rem",
+      }
+    }
   },
   plugins: [],
 }
